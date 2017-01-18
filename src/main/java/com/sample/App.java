6c93b0c02a221544cd4b86effc33b3cf2ad92051
zachxu65 <yxutech@yahoo.com>
2017-01-18 10:18:10 -0500

    test 1

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -1,5 +1,7 @@
-public class App{
-   public static void main(String[] args){
-       System.out.println("This is just a test.");
-   }
+package com.sample;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("This is just another test.");
+	}
 }
